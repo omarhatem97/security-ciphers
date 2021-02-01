3130c8d51c7da2d15762067014ac08dd12af71dd
omarhatem97 <omarhatem221@gmail.com>
2021-02-01 23:59:03 +0200

    implement auto mode key generation in veginer

--- veginer.py
+++ veginer.py
@@ -1,9 +1,16 @@
 
-def prepare_key(key, plain_len, mode):
+def prepare_key(key, plain, mode):
     res = ""
     if mode == 'repeating':
-        for i in range(plain_len):
+        for i in range(len(plain)):
             res+= key[i% len(key)]
+    else:
+        diff = len(plain) - len(key)
+        for i in range(len(key)):
+            res+= key[i]
+
+        for i in range(diff):
+            res += plain[i]
 
     return res
 
@@ -14,7 +21,7 @@ def veginer(key, plain, mode):
     takes key, plain , mode returns cipher
     """
     cipher = ""
-    key = prepare_key(key, len(plain), mode)
+    key = prepare_key(key, plain, mode)
 
     for i in range(len(plain)):
         cipher += chr((ord(key[i])-97 + ord(plain[i])-97)%26 +97)
@@ -23,13 +30,8 @@ def veginer(key, plain, mode):
 
 
 
-
-
-
-
-
-
 if __name__ == '__main__':
 
-    print(prepare_key('pie', 8, 'repeating'))
+    # print(prepare_key('pie', 8, 'repeating'))
+    print(prepare_key('aether','mdampuaf', 'auto'))
     print(veginer('pie', 'mdampuaf', 'repeating'))
